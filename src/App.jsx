71e420f8cd5755e66e2837f09b2e58967f770968
Vikram Shrivastav <vikrams13122004@gmail.com>
2025-07-17 22:14:47 +0530

    fix vercel bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import About from "./components/About.jsx"
 import Footer from "./components/Footer.jsx"
 import Hero from "./components/Hero.jsx"
-import Projects from "./components/projects.jsx"
+import Projects from "./components/Projects.jsx"
 import Header from "./components/Header.jsx"
-import Contact from "./components/contact.jsx"
+import Contact from "./components/Contact.jsx"
 import React from 'react'
 
 function App() {
